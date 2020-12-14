f7c1c41c6d62e4d2a3083ca8bf75bf988778e754
menumaxitacos <menumaxitacos@gmail.com>
2020-12-14 14:53:27 +0100

    start

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("arthur le jul")
+from tkinter import Tk, Button, Canvas, StringVar, IntVar
+
+fenetre_principale = Tk()
+fenetre_principale.title("Space invaders")
+fenetre_principale.geometry("800x600")
+
+fenetre_principale.mainloop()
+
