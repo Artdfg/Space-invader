a2ca4ce87bd47cf17a1279f56a17b9a5d77bd2d3
menumaxitacos <menumaxitacos@gmail.com>
2020-12-14 17:51:55 +0100

    fin premiere séance

--- main.py
+++ main.py
@@ -11,85 +11,77 @@ Todo : Canevas principal, label du score actuel, bouton qui lance la partie, bou
 from tkinter import Tk, Button, Canvas, Label, StringVar, IntVar, PhotoImage, filedialog, messagebox #Importation des biblio
 
 
-dim_fenetre_principale_x = 800
-dim_fenetre_principale_y = 600
-
 gif_dict = {}
 
-fenetre_principale = Tk()
-fenetre_principale.title("Space invaders")
-#fenetre_principale.geometry(str(dim_fenetre_principale_x) + "x" + str(dim_fenetre_principale_y))
 
 
-class jeu() #fenetre graphique
-
-
-
-def f_lancerjeu():
-    print("Le jeu se lance")
-    #Score = "0"
-    boutonJouer.destroy()
-    labelScore = Label(fenetre_principale, text = "Score")
-    labelScore.pack(side = 'top')
-
-def f_quit():
-    global Quitt
-    Quitt = Tk()
-    Quitt.title("Etes vous sur de quitter ?")
-    Quitt.geometry('300x100')
-    boutonOui = Button(Quitt,bg = 'red', text="Oui", command = quitter)
-    boutonNon = Button(Quitt,bg = 'blue', text ="Non", command = Quitt.destroy ) 
-    boutonOui.grid(row=1, column=1)
-    boutonNon.grid(row=1, column=2)
-    Quitt.mainloop()
-
-def quitter():
-    Quitt.destroy()
-    fenetre_principale.destroy()
-
-def f_affichage() :
-
-    ecran_jeu = Canvas(fenetre_principale , width = dim_fenetre_principale_x, height = dim_fenetre_principale_y*0.80 , bg='white')#creation d'un canvas, zone de dessin a linterieur de la fenetre tkinter auquel on peut ajouter des trucs
-    ecran_jeu.pack(padx = 5, pady = 5)#inclusion, affichage du canvas dans la fenetre
-
-
-
-
-    #Affichage image ecran menu
-    ecran_jeu.delete('all') # on efface la zone graphique
-    file_name = filedialog.askopenfilename(title="Ouvrir une image", filetypes=[('gif files','.gif'),('all files','.*')])
-    print (file_name)
-    img_ouverture = PhotoImage(file = file_name)
-    gif_dict[file_name] = img_ouverture  # référence
-    print (gif_dict)
+#fenetre graphique
+class jeu() :
     
-    """
-    ecran_jeu.delete('all') # on efface la zone graphique
 
-    img_ouverture = PhotoImage(file = "image_menu.gif")
-    """
+    def __init__ (self) : # crée ma fanetre du jeu space invader
 
-    ecran_jeu.create_image(0, 0, anchor = "nw", image = img_ouverture)
-    ecran_jeu.config(height = img_ouverture.height(), width = img_ouverture.width()) 
-    ecran_jeu.pack()
+        self.fenetre_principale = Tk()
+        self.fenetre_principale.title("Space invaders")
 
-    global boutonJouer 
-    boutonJouer = Button(fenetre_principale, text="Jouer !", fg = 'red', command = f_lancerjeu)
-    boutonJouer.pack()
+    
+    def f_lancerjeu(self): #lance le jeu apres avoir appuyé sur le bouton jouer
 
-
-    #Score = "0"
-    labelScore = Label(fenetre_principale, text = "Score")
-    labelScore.pack()
-
-    buttonJouer = Button(fenetre_principale, text="Jouer !", fg = 'red', command = f_lancerjeu)
-    buttonJouer.pack()
-
-    boutonQuit = Button(fenetre_principale, text="Quitter", activebackground = 'red', activeforeground='white', command= f_quit)
-    boutonQuit.pack(side= 'bottom')
-
-f_affichage()
+        print("Le jeu se lance")
+        #self.Score = "0"
+        self.boutonJouer.destroy()
+        self.labelScore = Label(self.fenetre_principale, text = "Score")
+        self.labelScore.pack(side = 'top')
 
 
-fenetre_principale.mainloop()
+    def f_quit(self): #fenetre confirmation quitter le jeu
+
+        self.Quitt = Tk()
+        self.Quitt.title("Etes vous sur de quitter ?")
+        self.Quitt.geometry('300x100')
+        self.boutonOui = Button(self.Quitt,bg = 'red', text="Oui", command = self.quitter) #lance la fct quitter
+        self.boutonNon = Button(self.Quitt,bg = 'blue', text ="Non", command = self.Quitt.destroy ) #renvoie a la fenetre principale
+        self.boutonOui.grid(row=1, column=1)
+        self.boutonNon.grid(row=1, column=2)
+        self.Quitt.mainloop()
+
+
+    def quitter(self): # quitte le jeu apres avoir cliqué sur oui
+
+        self.Quitt.destroy()
+        self.fenetre_principale.destroy()
+
+
+    def f_affichage(self) :
+
+        # canvas ecran de jeu
+        self.ecran_jeu = Canvas(self.fenetre_principale, bg='white')#creation d'un canvas, zone de dessin a linterieur de la fenetre tkinter auquel on peut ajouter des trucs
+        self.ecran_jeu.pack(padx = 5, pady = 5)#inclusion, affichage du canvas dans la fenetre
+
+        # affichage image menu
+        self.img_ouverture = PhotoImage(file = "image_menu.gif")
+        self.ecran_jeu.create_image(0, 0, anchor = "nw", image = self.img_ouverture)
+        self.ecran_jeu.config(height = self.img_ouverture.height(), width = self.img_ouverture.width()) 
+        self.ecran_jeu.pack()
+        
+        #bouton jouer
+        self.boutonJouer = Button(self.fenetre_principale, text="Jouer !", fg = 'red', command = self.f_lancerjeu)
+        self.boutonJouer.pack()
+
+        # bouton quitter
+        self.boutonQuit = Button(self.fenetre_principale, text="Quitter", activebackground = 'red', activeforeground='white', command= self.f_quit)
+        self.boutonQuit.pack(side= 'bottom')
+
+
+        self.fenetre_principale.mainloop()
+
+
+
+jouer = jeu()
+
+jouer.f_affichage()
+
+
+
+
 
