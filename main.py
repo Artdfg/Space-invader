33fb38b07ec187aca7c3e8310224479c948eacbb
Arthdfg <Arthur.laudien@cpe.fr>
2020-12-14 14:50:13 +0100

    Entete

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+"""
+Code UTF-8
+Created on Mon Dec 14
+TP3-4 Space Invader
+@Author : Roman Grim and Arthur Laudien
+Todo : Canevas principal, label du score actuel, bouton qui lance la partie, bouton qui quitte proprement, Alien qui bouge tout seul, Joueur qui bouge
+"""
 print("arthur le jul")
