2d4de4a2aced7ceceaa65007a8839a95446e4e07
Arthdfg <Arthur.laudien@cpe.fr>
2020-12-14 15:17:07 +0100

    Score+Bouttonjouer

--- main.py
+++ main.py
@@ -7,10 +7,22 @@ TP3-4 Space Invader
 Todo : Canevas principal, label du score actuel, bouton qui lance la partie, bouton qui quitte proprement, Alien qui bouge tout seul, Joueur qui bouge
 """
 print("arthur le jul")
-from tkinter import Tk, Button, Canvas, StringVar, IntVar
+from tkinter import Tk, Button, Canvas, Label, StringVar, IntVar #Importation des biblio
+
+def f_lancerjeu():
+    print("Le jeu se lance")
 
 fenetre_principale = Tk()
 fenetre_principale.title("Space invaders")
 fenetre_principale.geometry("800x600")
 
+Score = "0"()
+labelScore = Label(fenetre_principale, text = "Score")
+labelScore.pack()
+
+buttonJouer = Button(fenetre_principale, text="Jouer !", fg = 'red', command = f_lancerjeu)
+buttonJouer.pack()
+
+
 fenetre_principale.mainloop()
+
